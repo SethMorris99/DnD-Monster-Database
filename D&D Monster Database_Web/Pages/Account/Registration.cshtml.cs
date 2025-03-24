43898537619285dcf14d0f4664e6559e49d05d82
tanak <tanak.solanki05@gmail.com>
2025-03-23 23:12:19 -0500

    added some styling to the code for the registration page

--- D&D Monster Database_Web/Pages/Account/Registration.cshtml.cs
+++ D&D Monster Database_Web/Pages/Account/Registration.cshtml.cs
@@ -1,20 +1,44 @@
 using D_D_Monster_Database_Web.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
 
 namespace D_D_Monster_Database_Web.Pages.Account
 {
-    public class RegisterModel : PageModel
+    public class RegistrationModel : PageModel
     {
-        public Registration NewUser { get; set; } = new Registration(); 
-        
+        [BindProperty]
+        public RegisterUser User { get; set; } = new RegisterUser();
+
         public void OnGet()
-        {
-            NewUser.Username = "John";
-        }
-        public void OnPost()
         {
 
         }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            // Save to database (add database logic here)
+            return RedirectToPage("Sign_In");
+        }
+    }
+
+    public class RegisterUser
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [Required, EmailAddress]
+        public string Email { get; set; }
+
+        [Required, DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required, Compare("Password", ErrorMessage = "Passwords do not match")]
+        public string ConfirmPassword { get; set; }
     }
 }
