ec4f3b6961be8f842233f3c49926e779a49add9e
NotVersed <166773515+NotVersed@users.noreply.github.com>
2025-03-24 15:06:57 -0500

    Added requirement for user inputs

--- D&D Monster Database_Web/Model/Sign_In.cs
+++ D&D Monster Database_Web/Model/Sign_In.cs
@@ -1,8 +1,12 @@
-﻿namespace D_D_Monster_Database_Web.Model
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace D_D_Monster_Database_Web.Model
 {
     public class Sign_In
     {
+        [Required(ErrorMessage = "Please enter your username")]
         public string Username {  get; set; }
+        [Required(ErrorMessage = "Please enter your password")]
 
         public string Password { get; set; }
     }
