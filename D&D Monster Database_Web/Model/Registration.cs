a13d39257142867758545065a3fc94d31e9c6b20
tanak <tanak.solanki05@gmail.com>
2025-03-28 16:35:51 -0500

    Mades changes to the Registration,Layout, and index files

--- D&D Monster Database_Web/Model/Registration.cs
+++ D&D Monster Database_Web/Model/Registration.cs
@@ -7,11 +7,11 @@ namespace D_D_Monster_Database_Web.Model
         [Display(Name = "Username")]
         [Required(ErrorMessage = "Username is required")]
         public string Username { get; set; }
+        
         [Required(ErrorMessage = "Email is required")]
-
         public string Email { get; set; }
+        
         [Required(ErrorMessage = "Password is required")]
-
         public string Password { get; set; }
 
         [Display(Name = "Confirm Password")]
--- D&D Monster Database_Web/Pages/Account/Registration.cshtml.cs
+++ D&D Monster Database_Web/Pages/Account/Registration.cshtml.cs
@@ -1,13 +1,14 @@
 using D_D_Monster_Database_Web.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using System.ComponentModel.DataAnnotations;
+using Microsoft.Data.SqlClient;
+
 
 namespace D_D_Monster_Database_Web.Pages.Account
 {
     public class RegistrationModel : PageModel
     {
-        
+        [BindProperty]
         public Registration NewUser { get; set; }
 
         public void OnGet()
@@ -17,13 +18,39 @@ namespace D_D_Monster_Database_Web.Pages.Account
 
         public IActionResult OnPost()
         {
-            if (!ModelState.IsValid)
+            //Validate User Input
+            if (ModelState.IsValid)
             {
+                //Save to Database 
+                //1. Create a connection to the databse 
+                string connectionString = "Server=(localdb)\\MSSQLLocalDB; Database=MonsterDatabase;Trusted_Connection = True;";
+                SqlConnection conn = new SqlConnection(connectionString);
+
+                //2. Create a command to insert the data
+                string cmdText = "INSERT INTO SystemUser (SystemUserID, AccountTypeID,UserFirstName,UserLastName,UserProfileImage, UserEmail, UserPassword) VALUES (@SystemUserID, @AccountTypeID, @UserFirstName,@UserLastName,@UserProfileImage, @UserEmail, @UserPassword)";
+                SqlCommand cmd = new SqlCommand(cmdText, conn);
+                cmd.Parameters.AddWithValue("@SystemUserID", 1);
+                cmd.Parameters.AddWithValue("@AccountTypeID", 1);
+                cmd.Parameters.AddWithValue("@UserFirstName", NewUser.Username);
+                cmd.Parameters.AddWithValue("@UserLastName", NewUser.Username);
+                cmd.Parameters.AddWithValue("@UserProfileImage", "default.jpg");
+                cmd.Parameters.AddWithValue("@UserEmail", NewUser.Email);
+                cmd.Parameters.AddWithValue("@UserPassword", NewUser.Password);
+
+                //3. Execute the command 
+                cmd.ExecuteNonQuery();
+
+                //4. Close the connection 
+                conn.Close();
+
+                //Redirect to Sign_In Page
+                return RedirectToPage("/Account/Sign_In");
+            }
+            else
+            {
+                //Redirect the user 
                 return Page();
             }
-
-            // Save to database (add database logic here)
-            return RedirectToPage("Sign_In");
         }
     }
 }
