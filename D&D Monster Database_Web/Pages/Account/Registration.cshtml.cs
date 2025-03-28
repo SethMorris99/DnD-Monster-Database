19428f89e50bb921215b09630e2b51dd8539e1dd
tanak <tanak.solanki05@gmail.com>
2025-03-28 16:40:41 -0500

    rewrite some of the code

--- D&D Monster Database_Web/Pages/Account/Registration.cshtml.cs
+++ D&D Monster Database_Web/Pages/Account/Registration.cshtml.cs
@@ -24,25 +24,24 @@ namespace D_D_Monster_Database_Web.Pages.Account
                 //Save to Database 
                 //1. Create a connection to the databse 
                 string connectionString = "Server=(localdb)\\MSSQLLocalDB; Database=MonsterDatabase;Trusted_Connection = True;";
-                SqlConnection conn = new SqlConnection(connectionString);
+                
+                //make a local variable 
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    //2. Create a command to insert the data
+                    string cmdText = "INSERT INTO SystemUser (SystemUserID, AccountTypeID,UserFirstName,UserLastName,UserProfileImage, UserEmail, UserPassword) VALUES (@SystemUserID, @AccountTypeID, @UserFirstName,@UserLastName,@UserProfileImage, @UserEmail, @UserPassword)";
+                    SqlCommand cmd = new SqlCommand(cmdText, conn);
+                    cmd.Parameters.AddWithValue("@SystemUserID", 1);
+                    cmd.Parameters.AddWithValue("@AccountTypeID", 1);
+                    cmd.Parameters.AddWithValue("@UserFirstName", NewUser.Username);
+                    cmd.Parameters.AddWithValue("@UserLastName", NewUser.Username);
+                    cmd.Parameters.AddWithValue("@UserProfileImage", "default.jpg");
+                    cmd.Parameters.AddWithValue("@UserEmail", NewUser.Email);
+                    cmd.Parameters.AddWithValue("@UserPassword", NewUser.Password);
 
-                //2. Create a command to insert the data
-                string cmdText = "INSERT INTO SystemUser (SystemUserID, AccountTypeID,UserFirstName,UserLastName,UserProfileImage, UserEmail, UserPassword) VALUES (@SystemUserID, @AccountTypeID, @UserFirstName,@UserLastName,@UserProfileImage, @UserEmail, @UserPassword)";
-                SqlCommand cmd = new SqlCommand(cmdText, conn);
-                cmd.Parameters.AddWithValue("@SystemUserID", 1);
-                cmd.Parameters.AddWithValue("@AccountTypeID", 1);
-                cmd.Parameters.AddWithValue("@UserFirstName", NewUser.Username);
-                cmd.Parameters.AddWithValue("@UserLastName", NewUser.Username);
-                cmd.Parameters.AddWithValue("@UserProfileImage", "default.jpg");
-                cmd.Parameters.AddWithValue("@UserEmail", NewUser.Email);
-                cmd.Parameters.AddWithValue("@UserPassword", NewUser.Password);
-
-                //3. Execute the command 
-                cmd.ExecuteNonQuery();
-
-                //4. Close the connection 
-                conn.Close();
-
+                    //3. Execute the command 
+                    cmd.ExecuteNonQuery();
+                }
                 //Redirect to Sign_In Page
                 return RedirectToPage("/Account/Sign_In");
             }
