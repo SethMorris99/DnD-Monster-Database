569b7917bbb7c7c0c97b492648fd89a2c6d5dfd3
NotVersed <leestephens2004@icloud.com>
2025-04-09 14:40:44 -0500

    empty pages

---
+++ D&D Monster Database_Web/Pages/Monsters/BrowseMonsters.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace D_D_Monster_Database_Web.Pages.Monsters
+{
+    public class BrowseMonstersModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
