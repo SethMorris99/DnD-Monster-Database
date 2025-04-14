34856ff4d19c763bbb59552c48b396eb3c9bff7e
Seth <Seth@DESKTOP-466P884>
2025-04-14 00:08:55 -0500

    minor changes to home page, navbar displays correct options based on login status

--- D&D Monster Database_Web/Pages/Account/About.cshtml.cs
+++ D&D Monster Database_Web/Pages/Account/About.cshtml.cs
@@ -5,8 +5,19 @@ namespace D_D_Monster_Database_Web.Pages.Account
 {
     public class AboutModel : PageModel
     {
+        public string Layout { get; private set; }
+
         public void OnGet()
         {
+            // Check if the user is authenticated
+            if (User.Identity != null && User.Identity.IsAuthenticated)
+            {
+                Layout = "~/Pages/Shared/_Layout.Authenticated.cshtml";
+            }
+            else
+            {
+                Layout = "~/Pages/Shared/_Layout.cshtml";
+            }
         }
     }
 }
--- D&D Monster Database_Web/Pages/Index.cshtml.cs
+++ D&D Monster Database_Web/Pages/Index.cshtml.cs
@@ -12,9 +12,21 @@ namespace D_D_Monster_Database_Web.Pages
             _logger = logger;
         }
 
+        
+
+        public string Layout { get; private set; }
+
         public void OnGet()
         {
-
+            // Check if the user is authenticated
+            if (User.Identity != null && User.Identity.IsAuthenticated)
+            {
+                Layout = "~/Pages/Shared/_Layout.Authenticated.cshtml";
+            }
+            else
+            {
+                Layout = "~/Pages/Shared/_Layout.cshtml";
+            }
         }
     }
 }
