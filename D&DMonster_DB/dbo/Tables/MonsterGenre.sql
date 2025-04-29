67950ea51dc102f166fec92c7a582764d4060029
notversed <leestephens2004@icloud.com>
2025-04-29 07:58:54 -0500

    fixed a database issue where monstergenre wasnt using a composite key

--- D&DMonster_DB/dbo/Tables/MonsterGenre.sql
+++ D&DMonster_DB/dbo/Tables/MonsterGenre.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [dbo].[MonsterGenre] (
     [MonsterID] INT NOT NULL,
     [GenreID]   INT NOT NULL,
-    CONSTRAINT [PK_MonsterGenre] PRIMARY KEY CLUSTERED ([MonsterID] ASC),
+    CONSTRAINT [PK_MonsterGenre_1] PRIMARY KEY CLUSTERED ([MonsterID] ASC, [GenreID] ASC),
     CONSTRAINT [FK_MonsterGenre_Monster] FOREIGN KEY ([MonsterID]) REFERENCES [dbo].[Monster] ([MonsterID])
 );
 
